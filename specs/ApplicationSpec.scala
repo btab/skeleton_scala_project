9624b7dfc7ddac019c54b522f7dc61eb22281f3a
Andre Ben Hamou <andre@bluetheta.com>
2011-11-29 23:07:43 +0000

    Corrected sample spec.

--- specs/ApplicationSpec.scala
+++ specs/ApplicationSpec.scala
@@ -1,5 +1,10 @@
 package com.organization.project
 
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+
 class ApplicationSpec extends WordSpec with ShouldMatchers {
-  "Application" should { "have specs" }
+  "Application should have specs" in {
+    pending
+  }
 }
