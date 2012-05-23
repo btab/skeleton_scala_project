137765293d2bf6a0ac689d0ff5a0abb118927945
Andre Ben Hamou <andre@bluetheta.com>
2012-05-23 01:26:58 +0100

    Updated SBT run environment per the website.

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 
 # taken from https://github.com/harrah/xsbt/wiki/Setup
-java_options="-Dfile.encoding=UTF8 -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
+java_options="-Dfile.encoding=UTF8 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M"
 sbt_command="java $java_options -jar project/sbt_0-11-3.jar"
 
 case $1 in
